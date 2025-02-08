22aa16c90b4496563c4a323c7a446f6b1b797202
Sayak Mukherjee <99540921+sayak395@users.noreply.github.com>
2025-02-08 14:52:15 +0000

    Adjusted JC dynamics
    Fixed the dynamics of the JC model. Theoretical and experimental frequencies match.

--- Initial_Simulation.py
+++ Initial_Simulation.py
@@ -3,8 +3,7 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from qutip import sigmam, sigmap, sigmaz
-from qutip import (basis, destroy, mesolve, qeye,tensor)
+from qutip import (basis, destroy, mesolve, qeye,tensor, SESolver, sigmaz, sigmam, sigmap, fock)
 from scipy.signal import find_peaks
 
 matplotlib.use('TkAgg')
@@ -12,18 +11,17 @@ matplotlib.use('TkAgg')
 # System parameters
 wc = 1.0 * 2 * np.pi  # cavity frequency
 wa = 1.0 * 2 * np.pi  # atom frequency 
-g = 0.05 * 2 * np.pi # coupling strength
-
+g = 5 * 2 * np.pi # coupling strength
+#g = 0
 
 # Hilbert space dimension for the cavity
 n_cavity = 75
 
 # Operators
-sigma_minus = tensor(qeye(n_cavity), sigmam()) # atomic lowering operator
-sigma_plus = tensor(qeye(n_cavity), sigmap())   # atomic raising operator
-sigma_z = tensor(qeye(n_cavity), sigmaz())       # atomic z operator
+sigma_minus = tensor(qeye(n_cavity), sigmap()) # atomic lowering operator
+sigma_plus = sigma_minus.dag()   # atomic raising operator
+sigma_z = tensor(qeye(n_cavity), sigmaz().dag())     # atomic z operator
 a = tensor(destroy(n_cavity), qeye(2))   # cavity annihilation operator
-
 
 # Times for which the state should be evaluated
 times = np.linspace(0, 25, 50000)
@@ -38,24 +36,27 @@ H_experimental = wc * H_theoretical
 
 
 # Initial state: superposition of state
-psi0 = (tensor(basis(n_cavity, 2), basis(2, 0)) + tensor(basis(n_cavity, 1), basis(2, 1)))/(np.sqrt(2))
+psi0 = (tensor(fock(n_cavity, 1), basis(2, 1)) + tensor(fock(n_cavity, 2), basis(2, 0)))
+
 
 # Solve the Schrodinger equation
 result = mesolve(H_theoretical, psi0, tau, c_ops=[], e_ops=[a.dag() * a, sigma_plus * sigma_minus])
+#solver = SESolver(H_theoretical)
+#result = solver.run(psi0, tau, e_ops = [a.dag() * a, sigma_plus * sigma_minus])
 
 n_c = result.expect[0]
 n_a = result.expect[1]
 
 #psi_array = psi0.full()
 fig, axes = plt.subplots(1, 1, figsize=(10, 6))
-axes.plot(tau, n_c, label="Cavity")
-axes.plot(tau, n_a, label="Atom Excitation")
+axes.plot(result.times, n_c, label="Cavity")
+axes.plot(result.times, n_a, label="Atom Excitation")
 axes.grid(True)
 axes.legend(loc='best')
 axes.set_xlabel("Time (arb. units)")
+axes.set_xlim(0,5)
 axes.set_ylabel("Photon number")
-axes.set_title("Initial States: (1,g) + (0, e)")
-
+axes.set_title("Initial States: (2,g) + (1, e)")
 plt.savefig('JC_model.png')
 plt.show()
 
@@ -88,20 +89,23 @@ print(f"Peak Frequency: {peak_freq} rad/s, Amplitude: {peak_amplitude}")
 
 #Iterate and verify model
 
-n_list = np.arange(1,11,1)
+n_list = np.arange(1,21,1)
 
 theo_freq = g * np.sqrt(n_list) #theoretical values
 
 freq_list = []
 
+g = basis(2, 0)
+e = basis(2, 1)
+
 
 for n in n_list:
-    psi = tensor(basis(n_cavity, n), basis(2, 0)) #+ tensor(basis(n_cavity, n-1), basis(2, 1))
-    psi = psi.unit()
+    psi = tensor(fock(n_cavity, n), g) + tensor(fock(n_cavity, n-1), e)
+    #print(psi.dims)
     output = mesolve(H_theoretical, psi, tau, c_ops=[], e_ops=[a.dag() * a, sigma_plus * sigma_minus])
     n_c = output.expect[0]
 
-    peaks, _ = find_peaks(n_c, height=0)  # You might need to adjust the 'height' parameter based on your data
+    peaks, _ = find_peaks(n_c, height=0.5)  # You might need to adjust the 'height' parameter based on your data
 
     # Calculate time points of peaks
     peak_times = tau[peaks]
@@ -115,6 +119,9 @@ print(freq_list)
 plt.figure()
 plt.scatter(n_list, freq_list, marker = 'x', label = 'experimental frequency')
 plt.scatter(n_list, theo_freq, color = 'red', marker = 'x', label = 'theoretical prediction')
+plt.xlabel('Fock state of cavity')
+plt.ylabel('Frequency (arb. units)')
 plt.grid(True)
 plt.legend(loc='best')
+plt.savefig('Rabi_oscillations_2.png')
 plt.show()
