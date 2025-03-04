8837fa957ccdb4c92fe991b2e12a2926cd89ac5a
Sayak Mukherjee <99540921+sayak395@users.noreply.github.com>
2025-03-04 15:38:08 +0000

    Optimising the fidelity of the JC model

--- Driven_JC_model.py
+++ Driven_JC_model.py
@@ -1,76 +1,135 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from qutip import *
-import matplotlib
+from qutip import (mesolve, qeye, tensor, fock, Qobj, fidelity)
+from skopt import gp_minimize
+from skopt.space import Real
+from skopt.plots import plot_convergence
+from skopt.utils import use_named_args
 
-from matplotlib.animation import FFMpegWriter
-
-matplotlib.use('TkAgg')
 
 # System parameters
 wc = 1.0 * 2 * np.pi  # cavity frequency
 wa = 1.0 * 2 * np.pi  # atom frequency
-g = 0.05 * 2 * np.pi  # coupling strength
-zeta = 0.1 * 2 * np.pi  # coupling strength of ion-laser field
-epsilon = 0.1 * 2 * np.pi  # cavity-laser field coupling strength
-w0 = 1.0 * 2 * np.pi  # laser frequency
-
+g = 0.05 * 2 * np.pi # cavity-atom coupling
+f = 0.5 * 2 * np.pi # laser-atom coupling
+w0 = 0.8 * 2 * np.pi # laser driving frequency
 # Hilbert space dimension for the cavity
-n_cavity = 75
+n_cavity = 15
 
-# Operators
-sigma_minus = tensor(qeye(n_cavity), sigmam())  # atomic lowering operator
-sigma_plus = tensor(qeye(n_cavity), sigmap())  # atomic raising operator
-sigma_z = tensor(qeye(n_cavity), sigmaz())  # atomic z operator
-a = tensor(destroy(n_cavity), qeye(2))  # cavity annihilation operator
+sigma_z_np = np.array([[1 , 0],
+                   [0, -1]])
+sigma_z_alt = Qobj(sigma_z_np)
 
-# Times for which the state should be evaluated
-times = np.linspace(0, 100, 100)  # time array
+sigma_plus_np = np.array([[0, 1],
+                   [0, 0]])
+
+sigma_plus_alt = Qobj(sigma_plus_np)
+
+sigma_minus_np = np.transpose(sigma_plus_np)
+
+sigma_minus_alt = Qobj(sigma_minus_np)
+
+def create_a(n):
+
+    matrix = np.zeros((n, n))
+
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            if j == i - 1:
+                matrix[i , j] = np.sqrt(i)
+
+    return Qobj(np.transpose(matrix))
+
+
+###########################
+# Define the Operators
+###########################
+
+sigma_minus = tensor(qeye(n_cavity), sigma_minus_alt) # atomic lowering operator
+sigma_plus =  tensor(qeye(n_cavity), sigma_plus_alt)  # atomic raising operator
+sigma_z = tensor(qeye(n_cavity), sigma_z_alt)     # atomic z operator
+a = tensor(create_a(n_cavity), qeye(2))   # cavity annihilation operator
+spin_down = fock(2, 1)
+spin_up = fock(2, 0)
+
+delta_c = wc - w0
+delta_a = wa - w0
+
+times = np.arange(0, 10, 1e-5)
 tau = wc * times
 
-# Define time-dependent coefficients as strings for QuTiP
-H_drive = [
-    [sigma_minus, f"{zeta/wc} * exp(1j * {w0/wc} * t)"],
-    [sigma_plus, f"{zeta/wc} * exp(-1j * {w0/wc} * t)"],
-    [a.dag(), f"{epsilon/wc} * exp(-1j * {w0/wc} * t)"],
-    [a, f"{epsilon/wc} * exp(1j * {w0/wc} * t)"]
-]
+H_driven_RWA = delta_c * a.dag() * a + 0.5 * delta_a * sigma_z + g * (sigma_plus * a + sigma_minus * a.dag())
++ 0.5 * f * (sigma_plus + sigma_minus)
 
-# Hamiltonian with rotating wave approximation and driving fields
-H_theoretical = [a.dag() * a + 0.5 * (wa/wc) * sigma_z + (g/wc) * (a.dag() * sigma_minus + a * sigma_plus)] + H_drive
+H_driven_RWA = H_driven_RWA/wc
 
-# Initial state: atom in the ground state, cavity in a ground state
-psi0 = tensor(basis(n_cavity, 0), basis(2, 0))
+psi = tensor(fock(n_cavity, 3), spin_up)
 
-# Solve the Schrödinger equation
-result = mesolve(H_theoretical, psi0, tau, c_ops=[], e_ops=[a.dag() * a, sigma_plus * sigma_minus])
-output = mesolve(H_theoretical, psi0, tau, c_ops=[], e_ops=[])
+result = mesolve(H_driven_RWA, psi, tau, c_ops = [], e_ops = [a.dag() * a, sigma_plus * sigma_minus])
 
 n_c = result.expect[0]
 n_a = result.expect[1]
 
-# Plotting results
-fig, axes = plt.subplots(1, 1, figsize=(10, 6))
-axes.plot(times, n_c, label="Cavity photon number")
-axes.plot(times, n_a, label="Atom excitation number")
-axes.grid(True)
-axes.legend(loc='best')
-axes.set_xlabel("Time (arbitrary units)")
-axes.set_ylabel("Occupation number")
-axes.set_title("Occupation number with driving field")
-plt.savefig('driven_JC_model.png')
+plt.figure()
+plt.plot(tau, n_c, label = 'Cavity')
+plt.plot(tau, n_a, label = 'Atom Excited State')
+plt.grid(True)
+plt.xlabel('Time (arb. units)')
+plt.ylabel('Fock state')
+plt.legend(loc='best')
 plt.show()
 
-# Animation of the Wigner function (if necessary)
-fig = plt.figure(figsize=(6, 6))
-ax = fig.add_subplot(111, projection='3d')
-ax.view_init(elev=25.0)
-xvec = np.linspace(-3, 3, 200)
+psi_den = psi * psi.dag()
 
-writer = FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+output = mesolve(H_driven_RWA, psi_den, tau, c_ops = [], e_ops = [])
 
-rho_cavity = [ptrace(rho, 0) for rho in output.states]
-fig, ani = anim_wigner(rho_cavity, xvec, xvec, projection='3d', colorbar=True, fig=fig, ax=ax)
-ani.save('wigner_function_animation_driven.mp4', writer='ffmpeg', fps=15)
+target_state = tensor(fock(n_cavity, 1), spin_down)
 
+target_den = target_state * target_state.dag()
+
+fid = fidelity(output.states[-1], target_den)
+print("Fidelity:", fid)
+
+# Search space for Bayesian Optimization
+space  = [Real(0.1 * 2 * np.pi, 1.0 * 2 * np.pi, name='f'),
+          Real(0.5 * 2 * np.pi, 1.5 * 2 * np.pi, name='w0')]
+
+# General objective function that accepts named arguments
+def objective_general(f, w0):
+    delta_c = wc - w0
+    delta_a = wa - w0
+    H = (delta_c * a.dag() * a + 0.5 * delta_a * sigma_z + 
+         g * (sigma_plus * a + sigma_minus * a.dag()) +
+         0.5 * f * (sigma_plus + sigma_minus))
+    H /= wc
+    result = mesolve(H, psi_den, tau, c_ops=[], e_ops=[])
+    final_rho = result.states[-1]
+    fid = fidelity(target_den, final_rho)
+    return -fid  # return negative fidelity for minimization
+
+def objective(x):
+    return objective_general(x[0], x[1])
+
+# Perform Bayesian Optimization
+res = gp_minimize(objective, space, n_calls=30, random_state=0, verbose=True)
+
+# Now generate data for heatmap using the general function
+f_range = np.linspace(0.1, 1, 50) * 2 * np.pi
+w0_range = np.linspace(0.5, 1.5, 50) * 2 * np.pi
+fidelity_map = np.zeros((50, 50))
+
+for i, f_val in enumerate(f_range):
+    for j, w0_val in enumerate(w0_range):
+        fidelity_map[i, j] = -objective_general(f=f_val, w0=w0_val)  # negative to convert back to positive fidelity
+
+# Plot heatmap
+plt.figure(figsize=(10, 8))
+plt.imshow(fidelity_map, extent=(0.5, 1.5, 0.1, 1), origin='lower', aspect='auto', cmap='viridis')
+plt.colorbar(label='Fidelity')
+plt.title('Fidelity Heatmap')
+plt.xlabel('$w_0 / 2\pi$')
+plt.ylabel('$f / 2\pi$')
 plt.show()
+
+# Best parameters found
+print(f"Optimal f: {res.x[0]/(2*np.pi)} Hz, Optimal w0: {res.x[1]/(2*np.pi)} Hz")
